e35f85afef2d0e641202af1b9423ddb71ddba461
Klajdi Ndoci <Klajdindoci@gmail.com>
2023-11-02 15:54:40 +0100

    parameterizedTest

--- src/test/java/KlajdiNdoci/U5W1D2ApplicationTests.java
+++ src/test/java/KlajdiNdoci/U5W1D2ApplicationTests.java
@@ -2,13 +2,19 @@ package KlajdiNdoci;
 
 import KlajdiNdoci.entities.Menú;
 import KlajdiNdoci.entities.Ordine;
+import KlajdiNdoci.entities.Pizza;
 import lombok.extern.slf4j.Slf4j;
 import org.junit.jupiter.api.AfterAll;
 import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.CsvSource;
+import org.junit.jupiter.params.provider.ValueSource;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.context.annotation.AnnotationConfigApplicationContext;
+
+import java.util.List;
 
 import static org.junit.jupiter.api.Assertions.*;
 
@@ -44,6 +50,20 @@ class U5W1D2ApplicationTests {
 		double result = o1.calcImportoTotale(costoCoperto);
 		assertEquals(18.560000000000002, result);
 		log.info("Somma importo totale: " + result);
+	}
+
+	@Test
+	void testNumPizze(){
+		List<Pizza> pizzaList = menú.getPizzaList();
+		assertEquals(3, pizzaList.size());
+		log.info("Sono presenti " + pizzaList.size() + " pizze nel menu");
+	}
+
+	@ParameterizedTest
+	@CsvSource({"2, 18.56", "3, 21.56", "4, 24.56"})
+	void calcThreeTots(int a, double expectedResult){
+		double result = o1.calcImportoTotale(a);
+		assertEquals(expectedResult, result, 0.001);
 	}
 
 	@AfterAll
