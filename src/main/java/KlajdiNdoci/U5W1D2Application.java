21b84144bdfd05378ae785ebaf6458228616ec42
Klajdi Ndoci <Klajdindoci@gmail.com>
2023-11-02 14:40:21 +0100

    verifyArrayIsFilled test

--- src/main/java/KlajdiNdoci/U5W1D2Application.java
+++ src/main/java/KlajdiNdoci/U5W1D2Application.java
@@ -1,5 +1,9 @@
 package KlajdiNdoci;
 
+import KlajdiNdoci.entities.Menú;
+import KlajdiNdoci.entities.Ordine;
+import KlajdiNdoci.entities.Pizza;
+import KlajdiNdoci.entities.Tavolo;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
@@ -9,6 +13,5 @@ public class U5W1D2Application {
 
 	public static void main(String[] args) {
 		SpringApplication.run(U5W1D2Application.class, args);
-
 	}
 }
--- src/test/java/KlajdiNdoci/U5W1D2ApplicationTests.java
+++ src/test/java/KlajdiNdoci/U5W1D2ApplicationTests.java
@@ -1,13 +1,30 @@
 package KlajdiNdoci;
 
+import KlajdiNdoci.entities.Ordine;
+import lombok.extern.slf4j.Slf4j;
+import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.context.annotation.AnnotationConfigApplicationContext;
+
+import static org.junit.jupiter.api.Assertions.*;
 
 @SpringBootTest
+@Slf4j
 class U5W1D2ApplicationTests {
+	@Test
+	void verifyArrayIsFilled(){
+		AnnotationConfigApplicationContext ctx = new AnnotationConfigApplicationContext(U5W1D2Application.class);
+		Ordine o1 = (Ordine) ctx.getBean("getOrdine");
+		assertNotNull(o1.getPizze());
+		log.info("L'array non é nullo");
+		ctx.close();
+	}
 
 	@Test
 	void contextLoads() {
 	}
+
+
 
 }
